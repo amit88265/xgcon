e7115bb1b1ff61ef41ae6381cd2efafb4d0ba55d
amit88265 <amitkumar88265@gmail.com>
2019-08-02 12:07:21 +0530

    set base

--- src/container/Dashboard.js
+++ src/container/Dashboard.js
@@ -1,11 +1,29 @@
 import React,{Component} from 'react';
 
 class Dashboard extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={defaultTab:this.props.drawerTabs[0]}
+    }
     render() {
         return ( 
             <div>
-
+                 <div>
+                {
+                    this.props.drawerTabs.map((tab,index)=>{
+                        <div>{tab.split(" ").join("")}</div>
+                    })
+                }
             </div>
+            {
+                this.props.drawerTabs.map((tab,index)=>{
+                    if(tab==drawerTabs)
+                    return 
+                })
+            }
+            </div>
+           
          );
     }
 }
